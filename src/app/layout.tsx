dcdd850a7b3288c334b932bde2076453a6cc04d4
Abhiraj Bhowmick <abhirajbhowmick27@gmail.com>
2026-01-28 17:41:16 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
