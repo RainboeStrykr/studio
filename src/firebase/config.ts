58e23f6f7c06a4dd58e7ce918eee19ca90c0669b
Abhiraj Bhowmick <abhirajbhowmick27@gmail.com>
2026-01-28 17:55:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4174454283-88b34",
+  "appId": "1:1088423788806:web:cef6e91fc270d1a8855e87",
+  "apiKey": "AIzaSyCmcAJvYhPoHJ_vG7CmSW2s3Bs3yNtuAQs",
+  "authDomain": "studio-4174454283-88b34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1088423788806"
+};
